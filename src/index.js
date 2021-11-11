8680c200241fb98479c4e9ff1a96352a72c82243
yk-valefor <87688106+yk-valefor@users.noreply.github.com>
2021-11-11 14:13:02 +0900

    34まで

--- src/index.js
+++ src/index.js
@@ -13,13 +13,39 @@ const onClickAdd = () => {
   const li = document.createElement("li");
   li.innerText = inputText;
 
+  // button(完了)タグ生成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    // TODOを未完了リストから完了リストへ
+
+    // ★未完了リストから削除
+    deleteFromUncompleteList(completeButton.parentNode);
+  });
+
+  // button(削除)タグ生成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    // TODOを未完了リストから削除
+    deleteFromUncompleteList(deleteButton.parentNode);
+  });
+
   // divの中に各要素を設定
   div.appendChild(li);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
 
   //未完了リストに追加
   document.getElementById("uncomplete-list").appendChild(div);
 };
 
+//　【関数】未完了リストから指定の要素を削除
+const deleteFromUncompleteList = (target) => {
+  document.getElementById("uncomplete-list").removeChild(target);
+};
+
+//　追加ボタン
 document
   .getElementById("add-button")
   .addEventListener("click", () => onClickAdd());
